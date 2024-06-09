e4c414c35bdd482f5c264ce59c6a864cc0b05fcd
Daniel Abril <danbril1015@gmail.com>
2024-06-09 13:54:45 -0500

    fix cors

--- src/main/java/com/daniel/ntt/rest/RestApplication.java
+++ src/main/java/com/daniel/ntt/rest/RestApplication.java
@@ -2,6 +2,9 @@ package com.daniel.ntt.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RestApplication {
@@ -10,4 +13,14 @@ public class RestApplication {
 		SpringApplication.run(RestApplication.class, args);
 	}
 
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*").allowedHeaders("*");
+			}
+		};
+	}
 }
